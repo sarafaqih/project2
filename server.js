fa11f3bd09c426c6f80b72bc5b027a472a873f51
Noora08 <noora.ym8@gmail.com>
2025-01-20 02:43:31 +0300

    noora commit

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(morgan("dev")); // Logs the requests in the terminal
 // 3. CONNECTION TO DATABASE
 // =======================
 mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{console.log("Connected to DATABSE")})
-.catch(()=>{console.log("ERROR CONNECTING TO DB OMAR")})
+.then(()=>{console.log(`Connected to ${mongoose.connection.name} DATABSE.`)})
+.catch(()=>{console.log(`ERROR CONNECTING TO DB ${mongoose.connection.name}.`)})
 
 
 
